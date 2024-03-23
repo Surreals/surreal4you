88c9c52325be0549eb9c735e062a5b891fbda2a1
Marian Fedkovych <m.fed@intobi.com>
2024-03-23 07:10:22 +0200

    ее

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Info, Links, Title } from "./components";
 
 const App = () => {
   return (
-    <div className="bg-white container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl	mx-auto overflow-hidden">
+    <div className="bg-white container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl	mx-auto overflow-hidden bg-center	bg-no-repeat	bg-contain	" style={{backgroundImage: `linear-gradient(rgb(255 255 255 / 100%), rgb(255 255 255 / 100%)), url(src/assets/main.png)`}}>
       <Links/>
       <Title/>
       <Info/>
