9383adc8fd11256305759b53bfd34fadc9374a30
Marian Fedkovych <m.fed@intobi.com>
2024-04-01 21:19:17 +0300

    text-black

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <h1 className="md:rotate-0 rotate-90 pointer-events-none md:scale-y-50 scale-y-75 font-bold uppercase md:text-9xl text-8xl absolute inset-0 place-self-center tracking-widest	">
+    <h1 className="text-black md:rotate-0 rotate-90 pointer-events-none md:scale-y-50 scale-y-75 font-bold uppercase md:text-9xl text-8xl absolute inset-0 place-self-center tracking-wides">
       surreal
     </h1>
   );
