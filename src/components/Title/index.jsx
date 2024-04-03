f854d40325081872dabc4a533cf16f4243c8bd08
Marian Fedkovych <m.fed@intobi.com>
2024-04-03 20:32:15 +0300

    fix?

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
   return (
-    <h1 className="text-black md:rotate-0 rotate-90 pointer-events-none md:scale-y-50 scale-y-75 font-bold uppercase md:text-9xl text-8xl absolute inset-0 place-self-center tracking-wides">
+    <h1 className="text-black md:rotate-0 rotate-90 pointer-events-none md:scale-y-50 scale-y-75 font-bold uppercase md:text-9xl text-8xl md:absolute inset-0 place-self-center tracking-wides">
       surreal
     </h1>
   );
