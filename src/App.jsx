58e9d67fdf30bb31e6a61adcb8ed254313e98b48
Marian Fedkovych <m.fed@intobi.com>
2024-04-03 20:35:55 +0300

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Info, Links, Title } from "./components";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <div className=" bg-amber-50 container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl xl:max-w-6xl 2xl:max-w-full	mx-auto overflow-hidden bg-center	bg-no-repeat	bg-contain	" >
-      <Links/>
-      <Title/>
-      <Info/>
-    </div>
+    <>
+      <div className=" bg-amber-50 container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl xl:max-w-6xl 2xl:max-w-full	mx-auto overflow-hidden bg-center	bg-no-repeat	bg-contain	">
+        <Links />
+        <Title />
+        <Info />
+      </div>
+      <Analytics />
+    </>
   );
 };
 // style={{backgroundImage: `linear-gradient(rgb(255 255 255 / 100%), rgb(255 255 255 / 100%)), url(src/assets/main.png)`}}
