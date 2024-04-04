0c16862e5831efabd342920746bc008dc4c53fd2
Marian Fedkovych <m.fed@intobi.com>
2024-04-04 08:58:41 +0300

    lime

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 const App = () => {
   return (
     <>
-      <div className=" bg-amber-50 container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl xl:max-w-6xl 2xl:max-w-full	mx-auto overflow-hidden bg-center	bg-no-repeat	bg-contain	">
+      <div className=" bg-lime-100 container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl xl:max-w-6xl 2xl:max-w-full	mx-auto overflow-hidden bg-center	bg-no-repeat	bg-contain	">
         <Links />
         <Title />
         <Info />
