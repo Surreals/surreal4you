78db9c1102958972e5ab5b1c1884ebe3ec525fa2
Marian Fedkovych <m.fed@intobi.com>
2024-05-16 16:02:29 +0300

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
+import React, { useState, useEffect, useRef } from 'react';
 import { Info, Links, Title } from "./components";
 import { Analytics } from "@vercel/analytics/react";
 import mp4 from './assets/vid.mp4';
 import webm from './assets/vid.webm';
-// import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const videoEl = videoRef.current;
+    if (videoEl) {
+      const handleCanPlay = () => {
+        setIsLoading(false);
+      };
+      videoEl.addEventListener('canplaythrough', handleCanPlay);
+
+      // Cleanup function to remove the event listener
+      return () => {
+        videoEl.removeEventListener('canplaythrough', handleCanPlay);
+      };
+    }
+  }, []);
+
   return (
     <>
-      <div className=" container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl xl:max-w-6xl 2xl:max-w-full	mx-auto overflow-hidden bg-center	bg-no-repeat	bg-contain	">
+      <div className="container flex justify-between flex-col size-full relative text-center p-2 md:max-w-4xl xl:max-w-6xl 2xl:max-w-full mx-auto overflow-hidden bg-center bg-no-repeat bg-contain">
+        {isLoading && <div className="absolute z-50 w-full h-full flex items-center justify-center"></div>}
         <Links />
         <Title />
         <Info />
-        <video autoPlay muted loop style={{zIndex: '-1'}} className="bg-slate-50 hue-rotate-45 absolute blur-sm md:blur-0 -translate-x-1/2 -translate-y-3 md:-translate-x-1.5 md:-translate-y-1  w-auto min-w-full min-h-full max-w-none">
-          <source src={webm} type="video/webm"/>
+        {/* <video ref={videoRef} autoPlay muted loop style={{ zIndex: '-1' }} className="bg-slate-50 hue-rotate-45 absolute blur-sm md:blur-0 -translate-x-1/2 -translate-y-3 md:-translate-x-1.5 w-auto min-w-full min-h-full max-w-none">
+          <source src={webm} type="video/webm" />
           <source src={mp4} type="video/mp4" />
-        </video>
+        </video> */}
       </div>
       <Analytics />
       {/* <SpeedInsights /> */}
     </>
   );
 };
-// style={{backgroundImage: `linear-gradient(rgb(255 255 255 / 100%), rgb(255 255 255 / 100%)), url(src/assets/main.png)`}}
+
 export default App;
