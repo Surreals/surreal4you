b047afc84ae96863f19e1ab6e16aa887cdd6aad0
Marian Fedkovych <m.fed@intobi.com>
2024-08-29 11:35:13 +0300

    surrealovych

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const mainLinks = [
 ]
 
 export const sideLinks = [
-    {name: 'INSTAGRAM', url: 'https://www.instagram.com/surreal4you/'},
+    {name: 'INSTAGRAM', url: 'https://www.instagram.com/surrealovych/'},
     {name: 'TIKTOK', url: 'https://www.youtube.com/channel/UCK4Jk9WTq1hlpt5s6nWnuZA'},
     {name: 'TELEGRAM', url: 'https://t.me/surrealenko'},
     {name: 'OTHER', url: 'https://linktr.ee/surreal4you'},
