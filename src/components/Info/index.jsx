2e04b36c8626bcb9a21ee393c73b2691117aab34
[dyad] <marianfedkovich@gmail.com>
2025-07-15 00:33:36 +0300

    [dyad] Calculate age dynamically - wrote 3 file(s) + extra files edited outside of Dyad

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,9 +1,14 @@
+import { calculateAge } from "../../utils/dateUtils";
+
 const Info = () => {
+  const birthDate = "2000-10-14"; // October 14, 2000
+  const age = calculateAge(birthDate);
+
   return (
     <div className="w-full flex items-end justify-between">
       <p>HQ</p>
       <div className="text-left">
-        <p>23 y.o, Uzhhorod</p>
+        <p>{age} y.o, Uzhhorod</p>
         <p>Hip-hop Artist</p>
       </div>
       <div className="text-left">
